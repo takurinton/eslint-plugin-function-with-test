de93a79f2ba70991a9be59ceade31e86d97dae17
takurinton <takurinton@takurinton.dev>
2024-01-20 13:17:45 +0900

    chore refactor (#3)
    * Add getIsImported function to check if a function is imported
    
    * fix functionName to exportedName
    
    * comment

--- src/rules/index.ts
+++ src/rules/index.ts
@@ -1,14 +1,7 @@
 import { TSESLint } from "@typescript-eslint/utils";
 import { Errors } from "./types";
 import { messages } from "./messages";
-import {
-  getFilePath,
-  getImportDeclarationFromFilePath,
-  getRelativePath,
-  getTestFileNames,
-  isNodeModulesImport,
-  isSameFilePath,
-} from "./utils";
+import { getIsImported, getTestFileNames } from "./utils";
 
 /**
  * @description export されている全ての関数にテストを必ず書くようにするためのルール
@@ -66,61 +59,11 @@ export const requireTest: TSESLint.RuleModule<Errors, []> = {
                   (def.type === "Variable" &&
                     def.node.init?.type === "ArrowFunctionExpression")
                 ) {
-                  const functionName = exportedName;
-                  for (const testFileName of testFileNames!) {
-                    const importDeclarations =
-                      getImportDeclarationFromFilePath(testFileName);
-
-                    // 今の node の関数（functionName）が import されているかどうかを確認する
-                    // import されているかどうかの基準は
-                    // - import されているファイルのパスが一致しているかどうか
-                    // - import している関数名が一致しているかどうか
-                    for (const importDeclaration of importDeclarations) {
-                      if (importDeclaration.type !== "ImportDeclaration") {
-                        return;
-                      }
-
-                      const { source, specifiers } = importDeclaration;
-                      if (isNodeModulesImport(source.value)) {
-                        continue;
-                      }
-
-                      const relativePath = getRelativePath(
-                        testFileName,
-                        filename
-                      );
-
-                      // テストファイルのパスから、import しているファイルのパスを取得する
-                      // ./foo/bar/index.ts => ./foo/bar に変換している
-                      const filePath = getFilePath(relativePath);
-
-                      // import する値を取得
-                      // MEMO: source.value と source.raw の違い調べる
-                      const { value } = source;
-
-                      const samePath = isSameFilePath(value, filePath);
-
-                      // path が違ったら return
-                      if (!samePath) {
-                        continue;
-                      }
-
-                      // import をしているファイルの中に、今の node の関数が import されているかどうかを確認する
-                      for (const specifier of specifiers) {
-                        if (specifier.type !== "ImportSpecifier") {
-                          return;
-                        }
-
-                        const { imported } = specifier;
-
-                        // 関数名が同じだったら、import されているということなので、isImported を true にする
-                        // 複数ファイルに分散してる可能性や、同じファイル内で import されている可能性もあるので、return はしない
-                        if (imported.name === functionName) {
-                          isImported = true;
-                        }
-                      }
-                    }
-                  }
+                  isImported = getIsImported(
+                    exportedName,
+                    testFileNames!,
+                    filename
+                  );
                 }
               }
 
@@ -138,59 +81,13 @@ export const requireTest: TSESLint.RuleModule<Errors, []> = {
         const { declaration } = node;
         // function 宣言
         if (declaration?.type === "FunctionDeclaration") {
-          let isImported = false;
           const functionName = declaration?.id?.name;
 
-          for (const testFileName of testFileNames!) {
-            const importDeclarations =
-              getImportDeclarationFromFilePath(testFileName);
-
-            // 今の node の関数（functionName）が import されているかどうかを確認する
-            // import されているかどうかの基準は
-            // - import されているファイルのパスが一致しているかどうか
-            // - import している関数名が一致しているかどうか
-            for (const importDeclaration of importDeclarations) {
-              if (importDeclaration.type !== "ImportDeclaration") {
-                return;
-              }
-
-              const { source, specifiers } = importDeclaration;
-              if (isNodeModulesImport(source.value)) {
-                continue;
-              }
-
-              const relativePath = getRelativePath(testFileName, filename);
-
-              // テストファイルのパスから、import しているファイルのパスを取得する
-              // ./foo/bar/index.ts => ./foo/bar に変換している
-              const filePath = getFilePath(relativePath);
-
-              // import する値を取得
-              // MEMO: source.value と source.raw の違い調べる
-              const { value } = source;
-
-              const samePath = isSameFilePath(value, filePath);
-              // path が違ったら return
-              if (!samePath) {
-                continue;
-              }
-
-              // import をしているファイルの中に、今の node の関数が import されているかどうかを確認する
-              for (const specifier of specifiers) {
-                if (specifier.type !== "ImportSpecifier") {
-                  return;
-                }
-
-                const { imported } = specifier;
-
-                // 関数名が同じだったら、import されているということなので、isImported を true にする
-                // 複数ファイルに分散してる可能性や、同じファイル内で import されている可能性もあるので、return はしない
-                if (imported.name === functionName) {
-                  isImported = true;
-                }
-              }
-            }
-          }
+          const isImported = getIsImported(
+            functionName!,
+            testFileNames!,
+            filename
+          );
 
           if (!isImported) {
             context.report({
@@ -208,59 +105,13 @@ export const requireTest: TSESLint.RuleModule<Errors, []> = {
             "ArrowFunctionExpression" &&
           declaration.declarations[0].id.type === "Identifier"
         ) {
-          let isImported = false;
           const functionName = declaration.declarations[0].id.name;
 
-          for (const testFileName of testFileNames!) {
-            const importDeclarations =
-              getImportDeclarationFromFilePath(testFileName);
-
-            // 今の node の関数（functionName）が import されているかどうかを確認する
-            // import されているかどうかの基準は
-            // - import されているファイルのパスが一致しているかどうか
-            // - import している関数名が一致しているかどうか
-            for (const importDeclaration of importDeclarations) {
-              if (importDeclaration.type !== "ImportDeclaration") {
-                return;
-              }
-
-              const { source, specifiers } = importDeclaration;
-              if (isNodeModulesImport(source.value)) {
-                continue;
-              }
-
-              const relativePath = getRelativePath(testFileName, filename);
-
-              // テストファイルのパスから、import しているファイルのパスを取得する
-              // ./foo/bar/index.ts => ./foo/bar に変換している
-              const filePath = getFilePath(relativePath);
-
-              // import する値を取得
-              // MEMO: source.value と source.raw の違い調べる
-              const { value } = source;
-
-              const samePath = isSameFilePath(value, filePath);
-              // path が違ったら return
-              if (!samePath) {
-                continue;
-              }
-
-              // import をしているファイルの中に、今の node の関数が import されているかどうかを確認する
-              for (const specifier of specifiers) {
-                if (specifier.type !== "ImportSpecifier") {
-                  return;
-                }
-
-                const { imported } = specifier;
-
-                // 関数名が同じだったら、import されているということなので、isImported を true にする
-                // 複数ファイルに分散してる可能性や、同じファイル内で import されている可能性もあるので、return はしない
-                if (imported.name === functionName) {
-                  isImported = true;
-                }
-              }
-            }
-          }
+          const isImported = getIsImported(
+            functionName,
+            testFileNames!,
+            filename
+          );
 
           if (!isImported) {
             context.report({
